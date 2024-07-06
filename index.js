34c9b70e19045b07568ec3978d0544feb88eb9e7
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-07-05 22:17:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ exports.getDatabase = async function () {
     return {
       id: page.id,
       name: page.properties.Name.title[0]?.plain_text,
-      broski: page.properties.Tags.multi_select.map((tag) => tag.name),
+      tags: page.properties.Tags.multi_select.map((tag) => tag.name), 
+      // broski: page.properties.Tags.multi_select.map((tag) => tag.name), // CHQ: confirmed I can name properties however I want like so
       // createdStartDate: page.properties.Created.date.start,
       // createdEndDate: page.properties.Created.date.end,
       
