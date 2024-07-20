b49ef4e1b9c1c81cd844958e0a8cdf731951dd5f
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-07-20 04:08:46 -0400

    Create sampleData.ts
    Creating sample data to pull in through API and test from client.

---
+++ sampleData.ts
@@ -0,0 +1,49 @@
+type WordFrequencies = {
+  text: string;
+  value: number;
+};
+
+let myData: WordFrequencies[] = [
+  {
+    text: "test1",
+    value: 640,
+  },
+  {
+    text: "test2",
+    value: 20,
+  },
+  {
+    text: "test3",
+    value: 16,
+  },
+  {
+    text: "test4",
+    value: 170,
+  },
+  {
+    text: "test5",
+    value: 10,
+  },
+  {
+    text: "test6",
+    value: 540,
+  },
+  {
+    text: "test7",
+    value: 12,
+  },
+  {
+    text: "test8",
+    value: 777,
+  },
+  {
+    text: "test9",
+    value: 450,
+  },
+  {
+    text: "test10",
+    value: 19,
+  }
+];
+
+export default myData;
