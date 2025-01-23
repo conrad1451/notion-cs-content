05f43c3b9d41bfa7dfb762756baa082f45d5a499
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-01-23 14:24:45 -0500

    Create indexResCal.js

---
+++ indexResCal.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({ auth: process.env.RESCAL_PERSON_KEY });
+const databaseId = process.env.NOTION_RESCAL_PERSONAL_DATABASE;
+
+exports.getResCal = async function () {
+  const response = await notion.databases.query({ database_id: databaseId });
+
+  const responseResults = response.results.map((page) => {
+    return {
+      id: page.id,
+      name: page.properties.Name.title[0]?.plain_text,
+      tags: page.properties.Tags.multi_select.map((tag) => tag.name), 
+      // CHQ: unsure why the following lines did not work. Will debug later
+      // link: page.properties["Link"].url,
+      // area: page.properties.Area.select.map((select) => select.name), 
+      // area: page.properties.Area.area.map((select) => select.name), 
+      // created: page.properties.Created.date, 
+      // broski: page.properties.Tags.multi_select.map((tag) => tag.name), // CHQ: confirmed I can name properties however I want like so
+      // createdStartDate: page.properties.Created.date.start,
+      // createdEndDate: page.properties.Created.date.end,
+    };
+  });
+
+  return responseResults;
+};
+ 
+  
