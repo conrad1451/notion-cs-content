092b51e0037e4c567e8998d2d23c287da0e290e4
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-01-25 12:58:13 -0500

    chore: clean up commented out code.

--- indexResCal.js
+++ indexResCal.js
@@ -11,9 +11,6 @@ exports.getResCal = async function () {
     return {
       id: page.id,
       name: page.properties.Name.title[0]?.plain_text,
-      // source: page.properties["Source"].select.name,
-      // source: page.properties["Source"].select[0].name, 
-      // source: page.properties["Source"]?.type,
       source: page.properties["Source"]?.select.name, 
       area: page.properties["Area"]?.select.name, 
       // type: page.properties["Type"]?.select.name, 
@@ -21,8 +18,6 @@ exports.getResCal = async function () {
       tags: page.properties["Tags"].multi_select.map((tag) => tag.name), 
       // CHQ: unsure why the following lines did not work. Will debug later
       // link: page.properties["Link"].url,
-      // area: page.properties.Area.select.map((select) => select.name), 
-      // area: page.properties.Area.area.map((select) => select.name), 
       // created: page.properties.Created.date, 
       // broski: page.properties.Tags.multi_select.map((tag) => tag.name), // CHQ: confirmed I can name properties however I want like so
       // createdStartDate: page.properties.Created.date.start,
