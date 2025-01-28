d35d2f75b2cff3051acec7de86084a3cdbe1d46e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-01-28 00:51:47 -0500

    feat: uncomment lines of code

--- indexNutrition.js
+++ indexNutrition.js
@@ -7,7 +7,7 @@ const databaseId = process.env.NOTION_FOODTABLE_PERSONAL_DATABASE;
 exports.getFoodBrands = async function () {
   const response = await notion.databases.query({ database_id: databaseId });
 
-  const theResponseResults = response.results.map((page) => {
+  const responseResults = response.results.map((page) => {
     return {
       id: page.id,
       name: page.properties.Name.title[0]?.plain_text,
@@ -20,15 +20,15 @@ exports.getFoodBrands = async function () {
       
       // pageCreationTime: page.properties["Created time"]?.created_time, 
 
-      // source: page.properties["Source"]?.select.name, 
+      source: page.properties["Source"]?.select.name, 
       
       // sourceLink: page.properties["Link (If Applicable)"]?.url, 
       // sourceConnection: page.properties["Connection"]?.url, 
       
       // area: page.properties["Area"]?.select.name,  
  
-      // tags: page.properties["Food groups"].multi_select.map((foodgroup) => foodgroup.name), 
-      // tags: page.properties["Tags"].multi_select.map((tag) => tag.name), 
+      tags: page.properties["Food groups"].multi_select.map((foodgroup) => foodgroup.name), 
+      tags: page.properties["Tags"].multi_select.map((tag) => tag.name), 
       // CHQ: unsure why the following lines did not work. Will debug later
       // link: page.properties["Link"].url,
       // created: page.properties.Created.date, 
@@ -38,7 +38,7 @@ exports.getFoodBrands = async function () {
     };
   });
 
-  return theResponseResults;
+  return responseResults;
 };
  
   
