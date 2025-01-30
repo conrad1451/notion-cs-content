7b10b19066a870480fca90ead5ab1cf3bf96b78d
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-01-29 23:33:24 -0500

    Create indexJSONExport.js

---
+++ indexJSONExport.js
@@ -0,0 +1,14 @@
+exports.testObjInJSON = async function () {
+
+const foo = {
+  foundation: "Mozilla",
+  model: "box",
+  week: 45,
+  transport: "car",
+  month: 7,
+}; 
+
+  return foo;
+};
+ 
+  
